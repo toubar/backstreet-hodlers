6579158f6ff173bf33eac529eec061f2382a332d
aroiggarcia <alejandro.roiggarcia@infor.com>
2019-09-28 16:18:31 +0200

    - Changed not descriptive names. - Deleted comment.

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit} from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import * as appSettings from "tns-core-modules/application-settings";
+import {Constant} from "~/constant";
 
 @Component({
     selector: "Home",
@@ -7,16 +9,13 @@ import {Component, OnInit} from "@angular/core";
     styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-
-    constructor() {
-    }
+    private walletText;
 
     ngOnInit(): void {
     }
 
-    textFieldValue: string = "";
-
-    onButtonTap(): void {
-        console.log("Button was pressed");
+    setWallet() {
+        appSettings.setString(Constant.WALLET_KEY, this.walletText);
+        console.log("LOG: Wallet key has been saved: " + (this.walletText));
     }
 }
