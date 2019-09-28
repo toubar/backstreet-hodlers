ec95b08285b1590bc6e40da5caefe4036417846a
aroiggarcia <alejandro.roiggarcia@infor.com>
2019-09-28 15:08:59 +0200

    - ApplicationSettings working. - Example with boolean

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import {EventData} from "data/observable";
+import {Button} from "ui/button";
+import * as appSettings from "tns-core-modules/application-settings";
 
 @Component({
     selector: "Home",
@@ -7,10 +10,31 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+    TURNKEY = "isTurnedOn";
 
     constructor() {
     }
 
     ngOnInit(): void {
+        appSettings.setBoolean("isTurnedOn", true);
     }
+
+    onTap(args: EventData) {
+        let button = args.object as Button;
+        let isTurnedOn = appSettings.getBoolean(this.TURNKEY, false);
+        console.log((isTurnedOn));
+    }
+
+    onTapChangeValue(args: EventData) {
+        let isTurnedOn = appSettings.getBoolean(this.TURNKEY, false);
+        if (isTurnedOn) {
+            console.log("isON");
+            appSettings.setBoolean(this.TURNKEY, false);
+        }
+        else {
+            console.log("isOFF");
+            appSettings.setBoolean(this.TURNKEY, true);
+        }
+    }
+
 }
