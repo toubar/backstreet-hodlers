e15f44f534315da342ae06ca6420abcbefe293ef
Toubar <ahmed.toubar@broadcom.com>
2019-09-29 14:38:27 +0200

    - Open deliveries page dynamic list

--- app/new_delivery_home/new-delivery-home.component.ts
+++ app/new_delivery_home/new-delivery-home.component.ts
@@ -1,5 +1,8 @@
-import { Component} from "@angular/core";
+import {ItemEventData} from "tns-core-modules/ui/list-view"
+import {AfterViewInit, Component} from "@angular/core";
 import {RouterExtensions} from "nativescript-angular";
+import axios from "axios";
+import {IDelivery} from "~/types"
 
 @Component({
     selector: "new-delivery-home",
@@ -7,13 +10,177 @@ import {RouterExtensions} from "nativescript-angular";
     templateUrl: "./new-delivery-home.component.html",
     styleUrls: ['./new-delivery-home.component.css']
 })
-export class NewDeliveryHomeComponent {
+
+export class NewDeliveryHomeComponent implements AfterViewInit {
+
+    // deliveries: Array<IDelivery> = [];
 
     constructor(private routerExtensions: RouterExtensions) {
 
     }
 
-    onNewDeliveryButtonTap() {
-        this.routerExtensions.navigate(["/new-delivery-form"]);
+    ngAfterViewInit() {
+        console.log("LOG: NewDeliveryHomeComponent just rendered")
+        // axios.get("localhost:8080/")
+        //     .then((res) => {
+        //         // this.deliveries = res.data;
+        //     })
+        //     .catch((err)=> {
+        //         console.log(err.toString());
+        //     });
     }
+
+    onNewDeliveryButtonTap() {
+        this.routerExtensions.navigate(["/new-delivery-form"], {clearHistory: false});
+    }
+
+    // dummy data
+    dummyDeliveries: IDelivery[] = [
+        {
+            status: "OPEN",
+            id: "1",
+            initiator: "alberto",
+            deliverer: "alex",
+            from: "Prague 1",
+            to: "Prague 2",
+            description: "",
+            tokens: 5,
+        },
+        {
+            status: "OPEN",
+            id: "2",
+            initiator: "rey",
+            deliverer: "toubar",
+            from: "Prague 1",
+            to: "Prague 3",
+            description: "",
+            tokens: 6,
+        },
+        {
+            status: "OPEN",
+            id: "3",
+            initiator: "rey",
+            deliverer: "toubar",
+            from: "Prague 1",
+            to: "Prague 3",
+            description: "",
+            tokens: 2,
+        },
+        {
+            status: "OPEN",
+            id: "4",
+            initiator: "rey",
+            deliverer: "toubar",
+            from: "Prague 1",
+            to: "Prague 4",
+            description: "",
+            tokens: 3,
+        },
+        {
+            status: "OPEN",
+            id: "5",
+            initiator: "rey",
+            deliverer: "toubar",
+            from: "Prague 5",
+            to: "Prague 6",
+            description: "",
+            tokens: 6
+        },
+        {
+            status: "OPEN",
+            id: "6",
+            initiator: "rey",
+            deliverer: "toubar",
+            from: "Prague 1",
+            to: "Prague 7",
+            description: "",
+            tokens: 9,
+        },
+        {
+            status: "OPEN",
+            id: "7",
+            initiator: "rey",
+            deliverer: "toubar",
+            from: "Prague 3",
+            to: "Prague 7",
+            description: "",
+            tokens: 5,
+        },
+        {
+            status: "OPEN",
+            id: "8",
+            initiator: "rey",
+            deliverer: "toubar",
+            from: "Prague 4",
+            to: "Prague 1",
+            description: "",
+            tokens: 3,
+        },
+        {
+            status: "OPEN",
+            id: "9",
+            initiator: "rey",
+            deliverer: "toubar",
+            from: "Prague 8",
+            to: "Prague 2",
+            description: "",
+            tokens: 8,
+        },
+        {
+            status: "OPEN",
+            id: "10",
+            initiator: "rey",
+            deliverer: "toubar",
+            from: "Prague 1",
+            to: "Prague 1",
+            description: "",
+            tokens: 3,
+        },
+        // {
+        //     status: "COMPLETED",
+        //     id: "11",
+        //     initiator: "rey",
+        //     deliverer: "toubar",
+        //     from: "alberto",
+        //     to: "rey",
+        //     description: "Novakovych 8",
+        //     tokens: 12,
+        // },
+        // {
+        //     status: "IN_PROGRESS",
+        //     id: "12",
+        //     initiator: "rey",
+        //     deliverer: "toubar",
+        //     from: "alberto",
+        //     to: "rey",
+        //     description: "Novakovych 8",
+        //     tokens: 12,
+        // },
+        // {
+        //     status: "OPEN",
+        //     id: "13",
+        //     initiator: "rey",
+        //     deliverer: "toubar",
+        //     from: "alberto",
+        //     to: "rey",
+        //     description: "Novakovych 8",
+        //     tokens: 12,
+        // },
+        // {
+        //     status: "IN_PROGRESS",
+        //     id: "14",
+        //     initiator: "rey",
+        //     deliverer: "toubar",
+        //     from: "alberto",
+        //     to: "rey",
+        //     description: "Novakovych 8",
+        //     tokens: 12,
+        // },
+    ];
+
+    onItemTap(args: ItemEventData): void {
+        console.log("item tapped");
+    }
+
+    isBusy: boolean = true;
 }
